6cd990f85719cbfe2191e29ca47cd7bfa57149c5
ayan <ayanmishra19feb@gmail.com>
2024-12-15 14:54:20 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function changepns(){
+    document.getElementById('pn').src="../image/can-pepsi.png";
+    document.getElementById('bd').style.backgroundColor="#004B93";
+    document.getElementById('pns').style.filter="grayscale(0)";
+}
+function changepzs(){
+    document.getElementById('pn').src="../image/can-pepsi-zero-sugar.png";
+    document.getElementById('bd').style.backgroundColor="#004B93";
+    document.getElementById('pzs').style.filter="grayscale(0)";
+}
+function changepcf(){
+    document.getElementById('pn').src="../image/can-pepsi-caffeine-free.png";
+    document.getElementById('bd').style.backgroundColor="#004B93";
+    document.getElementById('pcf').style.filter="grayscale(0)";
+}
+function changepm(){
+    document.getElementById('pn').src="../image/can-pepsi-mango.png";
+    document.getElementById('bd').style.backgroundColor="#004B93";
+    document.getElementById('pm').style.filter="grayscale(0)";
+}
+function changepc(){
+    document.getElementById('pn').src="../image/can-pepsi-cherry-v2.png";
+    document.getElementById('bd').style.backgroundColor="#004B93";
+    document.getElementById('pc').style.filter="grayscale(0)";
+}
